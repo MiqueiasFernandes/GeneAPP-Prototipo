9ea22eb70a9bb9decac8682f43140709c0ffa208
MiqueiasFernandes <contatomiqueiasfernandes@hotmail.com>
2022-04-23 22:40:24 -0300

    Add files via upload

--- ncbi_salmon_quant.sh
+++ ncbi_salmon_quant.sh
@@ -5,37 +5,35 @@ echo "[1] $(date) verificando pacotes."
 if ! command -v salmon &> /dev/null
 then
     echo "instalar pacotes!"
-    echo "apt install sra-toolkit trimmomatic fastqc salmon"
+    echo "!apt install sra-toolkit trimmomatic fastqc salmon 1> install.log 2> install.err"
     exit
 fi
 
 tid=t$(date +%s)
 
 echo '[2.1] baixando o GTF ...'
-wget -O gene.$tid.gtf.gz $1 $> _2.1_transcripts.download.log
+wget -O gene.$tid.gtf.gz $1 1> _2.1_transcripts.download.log 2> _2.1_transcripts.download.err
 echo '[2.2] descompactando o GTF ...'
-gunzip gene.$tid.gtf.gz $> _2.2_transcripts.download.log
+gunzip gene.$tid.gtf.gz 1> _2.2_transcripts.download.log 2> _2.2_transcripts.download.err
 
 echo '[3.1] baixando os transcritos ...'
-wget -O cds.$tid.fa.gz $2 $> _3.1_transcripts.download.log
+wget -O cds.$tid.fa.gz $2 1> _3.1_transcripts.download.log 2> _3.1_transcripts.download.err
 echo '[3.2] descompactando os transcritos ...'
-gunzip cds.$tid.fa.gz $> _2.2_transcripts.unzip.log
+gunzip cds.$tid.fa.gz 1> _2.2_transcripts.unzip.log 2> _2.2_transcripts.unzip.err
 echo '[3.3] indexando os transcritos ...'
-salmon index -t cds.$tid.fa --index idx$tid $> _2.3_transcripts.index.log
+salmon index -t cds.$tid.fa --index idx$tid 1> _2.3_transcripts.index.log 2> _2.3_transcripts.index.err
 
 for x in $@
     do 
-        if [[ $x doesNotContain *","* ]]
+        if [[ `echo $x | grep ,` ]]
         then
-         continue
-        else
             RUN=`echo $x | cut -d, -f1`
             SAMPLE=`echo $x | cut -d, -f2`
             echo "$RUN: $SAMPLE" >> processed
             i=$( wc -l processed | tr -cs 0-9 , | cut -d, -f2 )
 
             echo "[4.$i.1] obtendo a amostra $SAMPLE pelo acesso $RUN no sra ..."
-            fastq-dump --split-3 $RUN $> _4.1_download.$RUN.$SAMPLE.log
+            fastq-dump --split-3 $RUN 1> _4.1_download.$RUN.$SAMPLE.log 2> _4.1_download.$RUN.$SAMPLE.err
             
             echo "[4.$i.2] fazendo controle de qualidade da amostra $SAMPLE com o TrimmomaticPE ..."
             !TrimmomaticPE \
@@ -43,15 +41,15 @@ for x in $@
                 $SAMPLE.1.fq $SAMPLE.1.unp.fq \
                 $SAMPLE.2.fq $SAMPLE.2.unp.fq \
                 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:36 \
-                $> _4.2_qc.$SAMPLE.log
+                1> _4.2_qc.$SAMPLE.log 2> _4.2_qc.$SAMPLE.err
             
             echo "[4.$i.3] reportando controle de qualidade da amostra $SAMPLE com fastqc ..."
             rm qc_$SAMPLE -rf && mkdir qc_$SAMPLE
-            fastqc $SAMPLE.1.fq $SAMPLE.2.fq -o qc_$SAMPLE $> _4.3_stats.$SAMPLE.log
+            fastqc $SAMPLE.1.fq $SAMPLE.2.fq -o qc_$SAMPLE 1> _4.3_stats.$SAMPLE.log 2> _4.3_stats.$SAMPLE.err
             
             echo "[4.$i.4] quantificando a amostra $SAMPLE com salmon ..."
             salmon quant -1 $SAMPLE.1.fq -2 $SAMPLE.2.fq \
-            -o quant_$SAMPLE --libType IU --index idx$tid $> _4.4_quant.$SAMPLE.log
+            -o quant_$SAMPLE --libType IU --index idx$tid 1> _4.4_quant.$SAMPLE.log 2> _4.4_quant.$SAMPLE.err
 
             echo "[4.$i.4] limpando dados de $SAMPLE ..."
             mkdir out_$SAMPLE
@@ -62,6 +60,6 @@ for x in $@
 done 
 
 echo '[5] compactando para RESULTS.zip ...'
-zip RESULTS.zip out_*/* *.log
+zip RESULTS.zip out_*/* *.log *.err
 
 echo $(date) terminado.
