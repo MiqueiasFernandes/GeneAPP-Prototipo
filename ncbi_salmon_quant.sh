eb33caadec63cb08e2758a8810cd84837175d9e4
MiqueiasFernandes <contatomiqueiasfernandes@hotmail.com>
2022-04-24 23:37:02 -0300

    Add files via upload

--- ncbi_salmon_quant.sh
+++ ncbi_salmon_quant.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "[1] $(date +%D.%H-%M-%S) prepando o ambiente..."
+echo "[  1  ] $(date +%D.%H-%M-%S) prepando o ambiente..."
 tid=t$(date +%s)
 mkdir results$tid && cd results$tid
 
@@ -17,7 +17,7 @@ for prog in sra-toolkit trimmomatic fastqc salmon bamtools rna-star
     do
     if ! command -v $prog 1> /dev/null 2> /dev/null
     then
-        echo "[1.$p] instalando o $prog ..."
+        echo "[1.$p  ] instalando o $prog ..."
         apt install $prog -y 1> _1.$p\_install.$prog.log 2> _1.$p\_install.$prog.err
         (( p=p+1 ))
     fi
@@ -38,7 +38,7 @@ for pkg in multiqc biopython deeptools
     do
     if [[ ! `pip list | grep $pkg` ]]
         then 
-        echo "[1.$p] instalando o $pkg ..."
+        echo "[1.$p  ] instalando o $pkg ..."
         pip install $pkg 1> _1.$p\_install.$pkg.log 2> _1.$p\_install.$pkg.err
         (( p=p+1 ))
     fi
@@ -61,23 +61,23 @@ if ! grep 'pysam.index(bamFile)' /usr/local/lib/python3.7/dist-packages/deeptool
     rm xtemp bamHandler.py
 fi
 
-echo '[2] importando a anotação ...'
-echo '[2.1] baixando o genoma ...'
+echo '[  2  ] $(date +%D.%H-%M-%S) importando genoma e a anotação ...'
+echo '[2.1  ] baixando o genoma ...'
 wget -O genoma.$tid.fa.gz $1 1> _2.1_genoma.download.log 2> _2.1_genoma.download.err
-echo '[2.2] descompactando o genoma ...'
+echo '[2.2  ] descompactando o genoma ...'
 gunzip genoma.$tid.fa.gz 1> _2.2_genoma.unzip.log 2> _2.2_genoma.unzip.err
-echo '[2.3] baixando o GTF ...'
+echo '[2.3  ] baixando o GTF ...'
 wget -O gene.$tid.gtf.gz $2 1> _2.3_gtf.download.log 2> _2.3_gtf.download.err
-echo '[2.4] descompactando o GTF ...'
+echo '[2.4  ] descompactando o GTF ...'
 gunzip gene.$tid.gtf.gz 1> _2.4_gtf.unzip.log 2> _2.4_gtf.unzip.err
 
-echo '[3] importando transcritos ...'
-echo '[3.1] baixando os transcritos ...'
+echo '[  3  ] $(date +%D.%H-%M-%S) importando transcritos ...'
+echo '[3.1  ] baixando os transcritos ...'
 wget -O cds.$tid.fa.gz $3 1> _3.1_transcripts.download.log 2> _3.1_transcripts.download.err
-echo '[3.2] descompactando os transcritos ...'
+echo '[3.2  ] descompactando os transcritos ...'
 gunzip cds.$tid.fa.gz 1> _3.2_transcripts.unzip.log 2> _3.2_transcripts.unzip.err
 
-echo '[3.3] filtrando os transcritos ...'
+echo '[3.3  ] filtrando os transcritos ...'
 echo "cds = 'cds.$tid.fa'" > script.py
 cat >> script.py << EOF 
 seqs = [(l.strip(), l[1:-1].split()) for l in open(cds).readlines() if l.startswith('>')]
@@ -108,7 +108,7 @@ EOF
 python3 script.py 1> _3.3_transcripts.filter.log 2> _3.3_transcripts.filter.err
 rm script.py
 
-echo '[3.4] extraindo sequencia de genes ...'
+echo '[3.4  ] extraindo sequencia de genes ...'
 echo "cds = 'cds.$tid.fa'" > script.py
 echo "genoma = 'genoma.$tid.fa'" >> script.py
 echo "gtf = 'gene.$tid.gtf'" >> script.py
@@ -134,13 +134,13 @@ EOF
 python3 script.py 1> _3.4_genes.extract.log 2> _3.4_genes.extract.err
 rm script.py
 mkdir idxgenes
-echo '[3.5] indexando sequencia de genes ...'
+echo '[3.5  ] indexando sequencia de genes ...'
 STAR --runMode genomeGenerate --genomeDir idxgenes  --genomeFastaFiles gene_seqs.fa 1> _3.5_genes.index.log 2> _3.5_genes.index.err
 
-echo '[3.6] indexando os transcritos ...'
+echo '[3.6  ] indexando os transcritos ...'
 salmon index -t cds.$tid.fa --index idx$tid 1> _3.6_transcripts.index.log 2> _3.6_transcripts.index.err
 
-echo '[4] quantificando amostras ...'
+echo '[  4  ] quantificando amostras ...'
 i=1
 for x in $@
     do 
@@ -149,7 +149,7 @@ for x in $@
             RUN=`echo $x | cut -d, -f1`
             SAMPLE=`echo $x | cut -d, -f2`
 
-            echo "[4.$i.1] obtendo a amostra $SAMPLE pelo acesso $RUN no sra ..."
+            echo "[4.$i.1] $(date +%D.%H-%M-%S) obtendo a amostra $SAMPLE pelo acesso $RUN no sra ..."
             fastq-dump --split-3 --minReadLen 80 $RUN 1> _4.$i.1_download.$RUN.$SAMPLE.log 2> _4.$i.1_download.$RUN.$SAMPLE.err
             
             echo "[4.$i.2] fazendo controle de qualidade da amostra $SAMPLE com o trimmomatic ..."
@@ -176,7 +176,7 @@ for x in $@
             echo "[4.$i.6] gerando arquivo de cobertura para a amostra $SAMPLE com deeptools ..."
             bamCoverage --bam Aligned.sortedByCoord.out.bam -o $SAMPLE.bw --binSize 3 1> _4.$i.6_cov.$SAMPLE.log 2> _4.$i.6_cov.$SAMPLE.err
 
-            echo "[4.$i.5] limpando dados de $SAMPLE ..."
+            echo "[4.$i.7] limpando dados de $SAMPLE ..."
             mkdir out_$SAMPLE
             cp quant_$SAMPLE/quant.sf out_$SAMPLE/$SAMPLE.quant.sf
             mv qc_$SAMPLE out_$SAMPLE
@@ -188,11 +188,11 @@ for x in $@
         fi
 done 
 
-echo '[5] executando o multiqc ...'
+echo '[  5  ] $(date +%D.%H-%M-%S) executando o multiqc ...'
 multiqc out_*/qc_* 1> _5_multiqc.log 2> _5_multiqc.err
 
-echo '[6] compactando para RESULTS.zip ...'
+echo '[  6  ] $(date +%D.%H-%M-%S) compactando para RESULTS.zip ...'
 zip RESULTS.zip out_*/*  multiqc_* *.log *.err 1> _6_zip.log 2> _6_zip.err
-cd .. && cp RESULTS.zip .
+cp RESULTS.zip ../ && cd ..
 
 echo $(date +%D.%H-%M-%S) terminado.
